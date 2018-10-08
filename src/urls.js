fd24567cf1a9865c37403082040ab18f32b570e6
pradumangoyal <pgoyal@ma.iitr.ac.in>
2018-10-08 22:52:59 +0530

    Add URLs

---
+++ src/urls.js
@@ -0,0 +1,19 @@
+export function urlBrandingText () {
+  return '/bootstrap/branding_text/'
+}
+
+export function urlBrandingImage () {
+  return '/bootstrap/branding_imagery/'
+}
+
+export function urlSiteInformation () {
+  return '/bootstrap/site_information/'
+}
+
+export function urlWhoAmI () {
+  return '/kernel/who_am_i/'
+}
+
+export function urlGetMaintainers () {
+  return '/kernel/maintainers/'
+}
