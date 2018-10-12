8766559e4628550968f4ae36acc223878615d57c
pradumangoyal <pgoyal@ma.iitr.ac.in>
2018-10-12 15:05:47 +0530

    Modify stylName property to import css modules from inlline.css

--- src/components/tile-card.js
+++ src/components/tile-card.js
@@ -2,19 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Card, Header, Icon, Image } from 'semantic-ui-react'
 
-import 'formula_one/src/css/app-header.css'
+import inline from 'formula_one/src/css/inline.css'
 
 const TileCard = props => {
   const { name, desc, className, iconName, imageUrl, ...otherProps } = props
   return (
     <Card className={`common-card ${className || ''}`} {...otherProps}>
-      <Card.Content styleName='padding-0'>
-        <div styleName='flex-column'>
-          <div styleName='flex margin-1em'>
+      <Card.Content styleName='inline.padding-0'>
+        <div styleName='inline.flex-column'>
+          <div styleName='inline.flex inline.margin-1em'>
             {imageUrl
               ? <Image src={imageUrl} width='28px' height='28px' />
               : <Icon name={iconName || 'group'} size='large' />}
-            <div styleName='flex-column margin-left-1_5em align-self-center'>
+            <div styleName='inline.flex-column inline.margin-left-1_5em inline.align-self-center'>
               <div>
                 <Header as='h4'>
                   {name}
