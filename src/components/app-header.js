0910df6ec08952989b705fa8d10e0f84cf73da9e
pradumangoyal <pgoyal@ma.iitr.ac.in>
2018-12-10 00:39:55 +0530

    Split `app-header` in smaller understandable components

--- src/components/app-header.js
+++ src/components/app-header.js
@@ -46,9 +46,11 @@ class AppHeader extends React.PureComponent {
   getMaintainersBranding () {
     return axios.get(urlMaintainersBranding())
   }
+
   getInstituteBranding () {
     return axios.get(urlInstituteBranding())
   }
+
   getAppBranding () {
     return axios.get(urlAppBranding(this.props.appName))
   }
@@ -93,13 +95,228 @@ class AppHeader extends React.PureComponent {
     this.setUser()
   }
 
+  /**
+   * Renders favicon on the tab
+   */
+  faviconRenderer = () => {
+    const { site, app, loaded } = this.state
+    const { mode } = this.props
+
+    // Wait for the 200 response from all the API's 
+    if (loaded) {
+      // If the mode of the header is 'app'
+      if (mode === 'app') {
+        // Renders favicon of app provided by backend, if it exists
+        if (app.assets && app.assets.favicon) {
+          return (
+            <Favicon
+              url={`static/${app.baseUrls.static}${app.assets &&
+                app.assets.favicon}`}
+            />
+          )
+        }
+
+        // Renders favicon of site provided by backend, if it exists and app favicon doesn't
+        else {
+          if (site.imagery && site.imagery.favicon) {
+            return <Favicon url={site.imagery.favicon} />
+          }
+        }
+      }
+
+      // By default, selected mode is 'site'
+      // Renders favicon of site provided by backend, if it exists
+      else if (site.imagery && site.imagery.favicon) {
+        return <Favicon url={site.imagery.favicon} />
+      }
+    }
+  }
+
+  /**
+   * Renders the logo on the left in the header 
+   */
+  headerLogoRenderer = () => {
+    const { site, app, loaded, } = this.state
+    const {
+      mode,
+    } = this.props
+
+    // Wait for the 200 response from all the APIs 
+    if (loaded){
+      // If the mode of the header is 'app'
+      if (mode === 'app') {
+        // Check if selected mode is 'app' and logo exists at backend in assets
+        // Return the app logo from backend as logo
+        if (app.assets && app.assets.logo) {
+          return <Image
+            src={`static/${app.baseUrls.static}${app.assets.logo}`}
+            inline
+            alt={app.nomenclature.verboseName}
+            styleName='header.site-logo'
+          />
+        }
+
+        // If app logo doesn't exist at backend,
+        // Check if site logo is present, and return the site logo as logo
+        else if (site.imagery.logo) {
+          return <Image
+            src={site.imagery.logo}
+            inline
+            alt={site.nomenclature.verboseName}
+            styleName='header.site-logo'
+          />
+        }
+
+        // If app logo as well as site logo doesn't exist at backend,
+        // Check if site wordmark is present, and return the site wordmark as logo
+        else if (site.imagery.wordmark) {
+          return <Image
+            src={site.imagery.wordmark}
+            inline
+            alt={site.nomenclature.verboseName}
+            styleName='header.site-logo'
+          />
+        }
+
+        // If app logo, site logo as well as site wordmark isn't present at backend,
+        // Return the site verboseName as logo
+        else return (
+          <div styleName='header.header-text'>
+            <Header as='h2'>{site.nomenclature.verboseName}</Header>
+          </div>
+        )
+      }
+      
+      //  By default, selected mode is 'site'
+      // Check if site logo is provided and set site logo as logo
+      else if(site.imagery.logo) {
+        return <Image
+          src={site.imagery.logo}
+          inline
+          alt={site.nomenclature.verboseName}
+          styleName='header.site-logo'
+        />
+      } 
+      
+      // If site logo is not present use site wordmark instead
+      else if(site.imagery.wordmark){
+        return <Image
+          src={site.imagery.wordmark}
+          inline
+          alt={site.nomenclature.verboseName}
+          styleName='header.site-logo'
+        />
+      }
+      
+      // If site wordmark is not present use site name instead
+      else {
+        return (
+        <div styleName='header.header-text'>
+          <Header as='h2'>{site.nomenclature.verboseName}</Header>
+        </div>)}
+
+  }}
+
+  /**
+   * Gives link to the logo on the left in the header
+   */
+  headerLeftLogoLinks = () => {
+    const { app, loaded } = this.state
+    const { appName, mode } = this.props
+
+    // Wait for the 200 response from all the APIs
+    // Only renders app baseURL from frontend config if 
+    // - selected mode is app
+    // - app logo exists (because if app logo doesn't exist, site logo will be on display and it should point to site)
+    // - app is present in common list of apps from backend and frontend i.e. app exists
+    if (loaded && mode === 'app' && app.assets && app.assets.logo && appDetails(appName).present) { 
+          return appDetails(appName).details.baseUrl
+    }
+    return '/'
+  }
+
+  /**
+   * Renders display name on left in the header
+   */
+  headerName = () => {
+    const { site, app, loaded } = this.state
+    const { mode } = this.props
+    
+    // Wait for 200 response of all branding APIs
+    if(loaded){
+      // If the mode of the header is 'app'
+      if(mode === 'app'){
+        // Render app verboseName from backend
+        return app.nomenclature.verboseName}
+
+      // By default, mode is 'site'
+      else{
+        // Render site verboseName from backend
+        return site.nomenclature.verboseName
+      }
+    }
+    // While retrieving data form APIs, render an empty string
+    return ''
+  }
+
+  /**
+   * Gives link to the display name on left
+   */
+  headerNameLink = () => {
+
+    const {  loaded } = this.state
+    const { appName, mode } = this.props
+    
+    // Wait for the 200 response from all the APIs
+    // Only renders app baseURL from frontend config if 
+    // - selected mode is app
+    // - app is present in common list of apps from backend and frontend i.e. app exists
+    if(loaded && mode === 'app' && appDetails(appName).present){  
+      return appDetails(appName).details.baseUrl
+    }
+    return '/'
+  }
+
+  /**
+   * Renders logo on right in case userDropdown is set to false
+   */
+  headerRightLogoRenderer = () => {
+
+    const { institute,loaded } = this.state
+    
+    // Wait for 200 response of all branding APIs
+    if(loaded){
+      // Check if institue logo exist renders institute logo
+      if(institute.imagery.logo){
+        return <Image
+          src={institute.imagery.logo}
+          styleName='inline.height-3_5em'
+          inline
+          alt={institute.text.name}
+        />
+      }
+
+      // If institute logo doesn't exist but wordmark exist, renders wordmark 
+      else if(institute.imagery.wordmark){
+        return 
+        <Image
+          src={institute.imagery.wordmark}
+          styleName='inline.height-3_5em'
+          inline
+          alt={institute.text.name}
+        />
+      }
+
+      // If neither of institute logo or wordmark exist, renders institute text name as heading 
+      else{
+        return  <Header as='h2'>{institute.text.name}</Header>}
+  }}
+
   render () {
-    const { site, institute, app, loaded, whoAmI } = this.state
+    const { site, loaded, whoAmI } = this.state
     const {
       userDropdown,
-      appName,
       hamburgerOptions,
-      mode,
       onSidebarClick,
       sideBarButton,
       sideBarVisibility,
@@ -113,28 +330,13 @@ class AppHeader extends React.PureComponent {
         alt={whoAmI && whoAmI['shortName']}
       />
     )
-
     return (
       <React.Fragment>
         <Segment attached='top' styleName='inline.padding-half'>
           <Helmet>
             <title>{loaded && site.nomenclature.verboseName}</title>
           </Helmet>
-          {loaded ? (
-            mode === 'app' && app.assets && app.assets.favicon ? (
-              <Favicon
-                url={`static/${app.baseUrls.static}${app.assets &&
-                  app.assets.favicon}`}
-              />
-            ) : (
-              site.imagery.favicon && <Favicon url={site.imagery.favicon} />
-            )
-          ) : (
-            false
-          )}
-          {loaded && mode === 'site' && site && site.imagery.favicon && (
-            <Favicon url={site.imagery.favicon} />
-          )}
+          {this.faviconRenderer()}
           <div styleName='header.header-container'>
             <div>
               {sideBarButton && (
@@ -157,86 +359,11 @@ class AppHeader extends React.PureComponent {
                   </span>
                 </button>
               )}
-              <a
-                href={
-                  loaded
-                    ? mode === 'app' && app.assets && app.assets.logo
-                      ? appDetails(appName).present
-                        ? appDetails(appName).details.baseUrl
-                        : '/'
-                      : '/'
-                    : '/'
-                }
-              >
-                {loaded ? (
-                  mode === 'app' ? (
-                    app.assets && app.assets.logo ? (
-                      <Image
-                        src={`static/${app.baseUrls.static}${app.assets.logo}`}
-                        inline
-                        alt={app.nomenclature.verboseName}
-                        styleName='header.site-logo'
-                      />
-                    ) : site.imagery.logo ? (
-                      <Image
-                        src={site.imagery.logo}
-                        inline
-                        alt={site.nomenclature.verboseName}
-                        styleName='header.site-logo'
-                      />
-                    ) : site.imagery.wordmark ? (
-                      <Image
-                        src={site.imagery.wordmark}
-                        inline
-                        alt={site.nomenclature.verboseName}
-                        styleName='header.site-logo'
-                      />
-                    ) : (
-                      <div styleName='header.header-text'>
-                        <Header as='h2'>{site.nomenclature.verboseName}</Header>
-                      </div>
-                    )
-                  ) : site.imagery.logo ? (
-                    <Image
-                      src={site.imagery.logo}
-                      inline
-                      alt={site.nomenclature.verboseName}
-                      styleName='header.site-logo'
-                    />
-                  ) : site.imagery.wordmark ? (
-                    <Image
-                      src={site.imagery.wordmark}
-                      inline
-                      alt={site.nomenclature.verboseName}
-                      styleName='header.site-logo'
-                    />
-                  ) : (
-                    <div styleName='header.header-text'>
-                      <Header as='h2'>{site.nomenclature.verboseName}</Header>
-                    </div>
-                  )
-                ) : (
-                  false
-                )}
-              </a>
-              <a
-                href={
-                  loaded
-                    ? mode === 'app'
-                      ? appDetails(appName).present &&
-                        appDetails(appName).details.baseUrl
-                      : '/'
-                    : '/'
-                }
-              >
+
+              <a href={this.headerLeftLogoLinks()}>{this.headerLogoRenderer()}</a>
+              <a href={this.headerNameLink()}>
                 <div styleName='header.header-text header.app-name'>
-                  <Header as='h2'>
-                    {loaded
-                      ? mode === 'app'
-                        ? app.nomenclature.verboseName
-                        : site.nomenclature.verboseName
-                      : ''}
-                  </Header>
+                  <Header as='h2'>{this.headerName()}</Header>
                 </div>
               </a>
             </div>
@@ -306,27 +433,7 @@ class AppHeader extends React.PureComponent {
                     href='/rest/login'
                   />
                 )
-              ) : loaded ? (
-                institute.imagery.logo ? (
-                  <Image
-                    src={institute.imagery.logo}
-                    styleName='inline.height-3_5em'
-                    inline
-                    alt={institute.text.name}
-                  />
-                ) : institute.imagery.wordmark ? (
-                  <Image
-                    src={institute.imagery.wordmark}
-                    styleName='inline.height-3_5em'
-                    inline
-                    alt={institute.text.name}
-                  />
-                ) : (
-                  <Header as='h2'>{loaded && institute.text.name}</Header>
-                )
-              ) : (
-                false
-              )}
+              ) : this.headerRightLogoRenderer()}
             </div>
           </div>
         </Segment>
