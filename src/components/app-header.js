ce68c867bd0ee4a341cd3a7127ad114469c84fb7
Praduman Goyal <goyal.praduman611@gmail.com>
2019-02-06 14:11:42 +0530

    Refactor code and change cursor to pointer on hovering user profile element

--- src/components/app-header.js
+++ src/components/app-header.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import axios from 'axios'
 import Helmet from 'react-helmet'
-import {Link} from 'react-router-dom'
-import {isBrowser} from 'react-device-detect'
+import { Link } from 'react-router-dom'
+import { isBrowser } from 'react-device-detect'
 import {
   Dropdown,
   Segment,
@@ -15,7 +15,7 @@ import {
 } from 'semantic-ui-react'
 import { map } from 'lodash'
 
-import { 
+import {
   urlAppBranding,
   urlInstituteBranding,
   urlSiteBranding,
@@ -108,14 +108,14 @@ class AppHeader extends React.PureComponent {
     const { site, app, loaded } = this.state
     const { mode } = this.props
 
-    // Wait for the 200 response from all the API's 
+    // Wait for the 200 response from all the API's
     if (loaded) {
       // If the mode of the header is 'app'
       if (mode === 'app') {
         // Renders favicon of app provided by backend, if it exists
         if (app.assets && app.assets.favicon) {
           return `/static/${app.baseUrls.static}${app.assets &&
-                app.assets.favicon}`
+            app.assets.favicon}`
         }
 
         // Renders favicon of site provided by backend, if it exists and app favicon doesn't
@@ -140,16 +140,17 @@ class AppHeader extends React.PureComponent {
   pageHead = () => {
     const { site, app, loaded } = this.state
     const { mode } = this.props
-    
+
     // Wait for 200 response of all branding APIs
-    if(loaded){
+    if (loaded) {
       // If the mode of the header is 'app'
-      if(mode === 'app'){
+      if (mode === 'app') {
         // Render app verboseName from backend
-        return <title>{app.nomenclature.verboseName}</title>}
+        return <title>{app.nomenclature.verboseName}</title>
+      }
 
       // By default, mode is 'site'
-      else{
+      else {
         // Render site verboseName from backend
         return <title>{site.nomenclature.verboseName}</title>
       }
@@ -159,16 +160,14 @@ class AppHeader extends React.PureComponent {
   }
 
   /**
-   * Returns the logo details for the left logo in the header 
+   * Returns the logo details for the left logo in the header
    loaded && */
-   headerLogoDetail = () => {
-    const { site, app, loaded, } = this.state
-    const {
-      mode,
-    } = this.props
+  headerLogoDetail = () => {
+    const { site, app, loaded } = this.state
+    const { mode } = this.props
 
-    // Wait for the 200 response from all the APIs 
-    if (loaded){
+    // Wait for the 200 response from all the APIs
+    if (loaded) {
       // If the mode of the header is 'app'
       if (mode === 'app') {
         // Check if selected mode is 'app' and logo exists at backend in assets
@@ -177,7 +176,7 @@ class AppHeader extends React.PureComponent {
           return {
             image: true,
             src: `/static/${app.baseUrls.static}${app.assets.logo}`,
-            text: app.nomenclature.verboseName 
+            text: app.nomenclature.verboseName
           }
         }
 
@@ -187,7 +186,7 @@ class AppHeader extends React.PureComponent {
           return {
             image: true,
             src: site.imagery.logo,
-            text: site.nomenclature.verboseName 
+            text: site.nomenclature.verboseName
           }
         }
 
@@ -197,74 +196,77 @@ class AppHeader extends React.PureComponent {
           return {
             image: true,
             src: site.imagery.wordmark,
-            text: site.nomenclature.verboseName 
+            text: site.nomenclature.verboseName
           }
         }
 
         // If app logo, site logo as well as site wordmark isn't present at backend,
         // Return the site verboseName as logo
-        else return {
+        else {
+          return {
             image: false,
             src: '',
-            text: site.nomenclature.verboseName 
+            text: site.nomenclature.verboseName
           }
+        }
       }
-      
+
       //  By default, selected mode is 'site'
       // Check if site logo is provided and set site logo as logo
-      else if(site.imagery.logo) {
+      else if (site.imagery.logo) {
         return {
           image: true,
           src: site.imagery.logo,
-          text: site.nomenclature.verboseName 
+          text: site.nomenclature.verboseName
         }
-      } 
-      
+      }
+
       // If site logo is not present use site wordmark instead
-      else if(site.imagery.wordmark){
+      else if (site.imagery.wordmark) {
         return {
           image: true,
           src: site.imagery.wordmark,
-          text: site.nomenclature.verboseName 
+          text: site.nomenclature.verboseName
         }
       }
-      
+
       // If site wordmark is not present use site name instead
       else {
         return {
           image: false,
           src: '',
-          text: site.nomenclature.verboseName 
+          text: site.nomenclature.verboseName
         }
       }
-
-  }}
-
+    }
+  }
 
   /**
-   * Renders the logo on the left in the header 
+   * Renders the logo on the left in the header
    loaded && */
   headerLogoRenderer = () => {
-    const { loaded, } = this.state
+    const { loaded } = this.state
 
-    // Wait for the 200 response from all the APIs 
-    if (loaded){
-      if(this.headerLogoDetail().image){
-        return <Image
+    // Wait for the 200 response from all the APIs
+    if (loaded) {
+      if (this.headerLogoDetail().image) {
+        return (
+          <Image
             src={this.headerLogoDetail().src}
             alt={this.headerLogoDetail().text}
             styleName='header.site-logo'
             inline
           />
-      }
-      else {
+        )
+      } else {
         return (
           <div styleName='header.header-text'>
             <Header as='h2'>{this.headerLogoDetail().text}</Header>
           </div>
         )
       }
-  }}
+    }
+  }
 
   /**
    * Gives link to the logo on the left in the header
@@ -274,12 +276,18 @@ class AppHeader extends React.PureComponent {
     const { appName, mode } = this.props
 
     // Wait for the 200 response from all the APIs
-    // Only renders app baseURL from frontend config if 
+    // Only renders app baseURL from frontend config if
     // - selected mode is app
     // - app logo exists (because if app logo doesn't exist, site logo will be on display and it should point to site)
     // - app is present in common list of apps from backend and frontend i.e. app exists
-    if (loaded && mode === 'app' && app.assets && app.assets.logo && appDetails(appName).present) { 
-          return appDetails(appName).details.baseUrl
+    if (
+      loaded &&
+      mode === 'app' &&
+      app.assets &&
+      app.assets.logo &&
+      appDetails(appName).present
+    ) {
+      return appDetails(appName).details.baseUrl
     }
     return '/'
   }
@@ -290,16 +298,17 @@ class AppHeader extends React.PureComponent {
   headerName = () => {
     const { site, app, loaded } = this.state
     const { mode } = this.props
-    
+
     // Wait for 200 response of all branding APIs
-    if(loaded){
+    if (loaded) {
       // If the mode of the header is 'app'
-      if(mode === 'app'){
+      if (mode === 'app') {
         // Render app verboseName from backend
-        return app.nomenclature.verboseName}
+        return app.nomenclature.verboseName
+      }
 
       // By default, mode is 'site'
-      else{
+      else {
         // Render site verboseName from backend
         return site.nomenclature.verboseName
       }
@@ -312,15 +321,14 @@ class AppHeader extends React.PureComponent {
    * Gives link to the display name on left
    */
   headerNameLink = () => {
-
-    const {  loaded } = this.state
+    const { loaded } = this.state
     const { appName, mode } = this.props
-    
+
     // Wait for the 200 response from all the APIs
-    // Only renders app baseURL from frontend config if 
+    // Only renders app baseURL from frontend config if
     // - selected mode is app
     // - app is present in common list of apps from backend and frontend i.e. app exists
-    if(loaded && mode === 'app' && appDetails(appName).present){  
+    if (loaded && mode === 'app' && appDetails(appName).present) {
       return appDetails(appName).details.baseUrl
     }
     return '/'
@@ -330,25 +338,26 @@ class AppHeader extends React.PureComponent {
    * Renders logo on right in case userDropdown is set to false
    */
   headerRightLogoRenderer = () => {
+    const { institute, loaded } = this.state
 
-    const { institute,loaded } = this.state
-    
     // Wait for 200 response of all branding APIs
-    if(loaded){
+    if (loaded) {
       // Check if institue logo exist renders institute logo
-      if(institute.imagery.logo){
-        return <Image
-          src={institute.imagery.logo}
-          styleName='inline.height-3_5em'
-          inline
-          alt={institute.text.name}
-        />
+      if (institute.imagery.logo) {
+        return (
+          <Image
+            src={institute.imagery.logo}
+            styleName='inline.height-3_5em'
+            inline
+            alt={institute.text.name}
+          />
+        )
       }
 
-      // If institute logo doesn't exist but wordmark exist, renders wordmark 
-      else if(institute.imagery.wordmark){
-        return 
-        <Image
+      // If institute logo doesn't exist but wordmark exist, renders wordmark
+      else if (institute.imagery.wordmark) {
+        return
+          <Image
           src={institute.imagery.wordmark}
           styleName='inline.height-3_5em'
           inline
@@ -356,23 +365,31 @@ class AppHeader extends React.PureComponent {
         />
       }
 
-      // If neither of institute logo or wordmark exist, renders institute text name as heading 
-      else{
-        return  <Header as='h2'>{institute.text.name}</Header>}
-  }}
+      // If neither of institute logo or wordmark exist, renders institute text name as heading
+      else {
+        return <Header as='h2'>{institute.text.name}</Header>
+      }
+    }
+  }
 
   user = () => {
-    const {whoAmI} =this.state
-    if(whoAmI && whoAmI['displayPicture'] !== '' && whoAmI['displayPicture']){
-      return <Image
-      avatar
-      src={whoAmI['displayPicture']}
-      alt={whoAmI && whoAmI['fullName'][0]}
-      style={{background: getThemeObject().hexCode}}
-      styleName='header.popup'
-    />
+    const { whoAmI } = this.state
+    if (whoAmI && whoAmI['displayPicture'] !== '' && whoAmI['displayPicture']) {
+      return (
+        <Image
+          avatar
+          src={whoAmI['displayPicture']}
+          alt={whoAmI && whoAmI['fullName'][0]}
+          style={{ background: getThemeObject().hexCode }}
+          styleName='header.popup'
+        />
+      )
     }
-    return <span styleName='header.popup'><DefaultDP name={whoAmI && whoAmI.fullName} dummy={{}} /></span>
+    return (
+      <span styleName='header.popup'>
+        <DefaultDP name={whoAmI && whoAmI.fullName} dummy={{}} />
+      </span>
+    )
   }
 
   render () {
@@ -394,7 +411,7 @@ class AppHeader extends React.PureComponent {
           <Helmet>
             {loaded && this.pageHead()}
             {loaded && <link rel='icon' href={this.faviconUrlRenderer()} />}
-            <meta name="theme-color" content={getThemeObject().hexCode} />
+            <meta name='theme-color' content={getThemeObject().hexCode} />
           </Helmet>
           <div styleName='header.header-container'>
             <div>
@@ -422,12 +439,13 @@ class AppHeader extends React.PureComponent {
               <Link to={this.headerLeftLogoLinks()}>
                 {this.headerLogoRenderer()}
               </Link>
-              {isBrowser && 
-              <Link to={this.headerNameLink()}>
-                <div styleName='header.header-text header.app-name'>
-                  <Header as='h2'>{this.headerName()}</Header>
-                </div>
-              </Link>}
+              {isBrowser && (
+                <Link to={this.headerNameLink()}>
+                  <div styleName='header.header-text header.app-name'>
+                    <Header as='h2'>{this.headerName()}</Header>
+                  </div>
+                </Link>
+              )}
             </div>
             {middle && middle}
             <div styleName='header.user-area-style'>
@@ -436,18 +454,24 @@ class AppHeader extends React.PureComponent {
                 whoAmI ? (
                   <React.Fragment>
                     <Dropdown
-                      trigger={<span><Icon name='bell outline' color='grey' size='large' /></span>}
+                      trigger={
+                        <span>
+                          <Icon name='bell outline' color='grey' size='large' />
+                        </span>
+                      }
                       pointing='top right'
                       icon={null}
                       styleName='inline.margin-right-half'
                     >
                       <Dropdown.Menu>
                         <Dropdown.Item>
-                            <Message negative icon='hourglass half' 
-                              header='Work in progress' 
-                              content='Notifications are not availaible at this stage.' 
-                            />
-                      </Dropdown.Item>
+                          <Message
+                            negative
+                            icon='hourglass half'
+                            header='Work in progress'
+                            content='Notifications are not availaible at this stage.'
+                          />
+                        </Dropdown.Item>
                       </Dropdown.Menu>
                     </Dropdown>
                     <Popup
@@ -460,20 +484,27 @@ class AppHeader extends React.PureComponent {
                     >
                       <div styleName='inline.flex-column'>
                         <div styleName='inline.flex inline.margin-1em'>
-                        {whoAmI && whoAmI.displayPicture && whoAmI.displayPicture !== '' ? 
-                        <img
-                            src={whoAmI.displayPicture}
-                            width='64px'
-                            height='64px'
-                            style={{  borderRadius: '32px', 
-                                      background: getThemeObject().hexCode 
-                                  }}
-                            alt='user'
-                          /> : <DefaultDP name={whoAmI && whoAmI.fullName} 
-                                size='3em' 
-                                styleName= 'header.popup' />}
-                          <div 
-                            styleName='inline.flex-column inline.margin-left-1_5em inline.align-self-center'>
+                          {whoAmI &&
+                          whoAmI.displayPicture &&
+                          whoAmI.displayPicture !== '' ? (
+                            <img
+                                src={whoAmI.displayPicture}
+                                width='64px'
+                                height='64px'
+                                style={{
+                                  borderRadius: '32px',
+                                  background: getThemeObject().hexCode
+                                }}
+                                alt='user'
+                              />
+                            ) : (
+                              <DefaultDP
+                                name={whoAmI && whoAmI.fullName}
+                                size='3em'
+                                styleName='header.popup'
+                              />
+                            )}
+                          <div styleName='inline.flex-column inline.margin-left-1_5em inline.align-self-center'>
                             <div>
                               <Header as='h4'>
                                 {whoAmI.fullName}
@@ -484,17 +515,48 @@ class AppHeader extends React.PureComponent {
                             </div>
                           </div>
                         </div>
-                        <Button.Group labeled icon basic vertical attached='bottom'>
-                          <Button icon='home' content='Home' as={Link} to='/' 
-                            styleName='inline.text-align-left'/>
-                          <Button icon='setting' content='Settings' as={Link} 
-                            to='/settings' styleName='inline.text-align-left'/>
-                          <Button icon='help' content='Helpcentre' as={Link} 
-                            to={`/helpcentre${mode==='app' ? `?app=${encodeURIComponent(appDetails(appName).details.nomenclature.verboseName)}` : ''}`} 
-                            styleName='inline.text-align-left'/>
-                          <Button icon='sign out' content='Log out' as={Link} 
-                            to='/auth/logout' styleName='inline.text-align-left'
-                            />
+                        <Button.Group
+                          labeled
+                          icon
+                          basic
+                          vertical
+                          attached='bottom'
+                        >
+                          <Button
+                            icon='home'
+                            content='Home'
+                            as={Link}
+                            to='/'
+                            styleName='inline.text-align-left'
+                          />
+                          <Button
+                            icon='setting'
+                            content='Settings'
+                            as={Link}
+                            to='/settings'
+                            styleName='inline.text-align-left'
+                          />
+                          <Button
+                            icon='help'
+                            content='Helpcentre'
+                            as={Link}
+                            to={`/helpcentre${
+                              mode === 'app'
+                                ? `?app=${encodeURIComponent(
+                                  appDetails(appName).details.nomenclature
+                                    .verboseName
+                                )}`
+                                : ''
+                            }`}
+                            styleName='inline.text-align-left'
+                          />
+                          <Button
+                            icon='sign out'
+                            content='Log out'
+                            as={Link}
+                            to='/auth/logout'
+                            styleName='inline.text-align-left'
+                          />
                         </Button.Group>
                       </div>
                     </Popup>
@@ -509,7 +571,9 @@ class AppHeader extends React.PureComponent {
                     to='/auth/login'
                   />
                 )
-              ) : loaded && this.headerRightLogoRenderer()}
+              ) : (
+                loaded && this.headerRightLogoRenderer()
+              )}
             </div>
           </div>
         </Segment>
