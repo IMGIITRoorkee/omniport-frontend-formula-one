c48a9e669b59343f7551d81bb27d00548b5c1320
Sweety Gupta <root@Sweetys-MacBook-Pro.local>
2022-05-17 02:49:38 +0530

    Redirect unauthenticated user to login page on 404

--- src/components/no-match.js
+++ src/components/no-match.js
@@ -2,20 +2,40 @@ import React from 'react'
 import { isBrowser } from 'react-device-detect'
 import { Container } from 'semantic-ui-react'
 import { Scrollbars } from 'react-custom-scrollbars'
+import { Redirect } from 'react-router'
 
 import {
   AppHeader,
   AppFooter,
   AppMain,
   ErrorDart,
-  ErrorRabbit
+  ErrorRabbit,
+  urlWhoAmI,
 } from 'formula_one'
 
 import main from '../css/app.css'
 import blocks from '../css/no-match.css'
 
 export default class NoMatch extends React.PureComponent {
-  render () {
+  state= {
+    whoAmI: null,
+  }
+  setUser = () => {
+    axios
+      .get(urlWhoAmI())
+      .then(res => {
+        this.setState({
+          whoAmI: res.data,
+        })
+      })
+      .catch(() => {
+        this.setState({
+          whoAmI: null,
+        })
+      })
+  }
+  render() {
+    const { whoAmI } = this.state
     const creators = [
       {
         name: 'Dhruv Bhanushali',
@@ -35,21 +55,29 @@ export default class NoMatch extends React.PureComponent {
     ]
     return (
       <React.Fragment>
-        <div styleName='main.app'>
-          <AppHeader mode='site' appName='links' userDropdown />
-          <AppMain>
-            <div styleName='main.app-main'>
-              <Scrollbars autoHide>
-                <div styleName='blocks.app-wrapper'>
-                  <Container styleName='blocks.main'>
-                    {isBrowser ? <ErrorDart /> : <ErrorRabbit />}
-                  </Container>
+        {whoAmI ? (
+          <React.Fragment>
+            <div styleName='main.app'>
+              <AppHeader mode='site' appName='links' userDropdown />
+              <AppMain>
+                <div styleName='main.app-main'>
+                  <Scrollbars autoHide>
+                    <div styleName='blocks.app-wrapper'>
+                      <Container styleName='blocks.main'>
+                        {isBrowser ? <ErrorDart /> : <ErrorRabbit />}
+                      </Container>
+                    </div>
+                  </Scrollbars>
                 </div>
-              </Scrollbars>
+              </AppMain>
+              <AppFooter creators={creators} />
             </div>
-          </AppMain>
-          <AppFooter creators={creators} />
-        </div>
+          </React.Fragment>
+        ) : (
+          <Redirect
+            to={`/auth/login?next=${window.location.pathname}${window.location.search}`}
+          />
+        )}
       </React.Fragment>
     )
   }
