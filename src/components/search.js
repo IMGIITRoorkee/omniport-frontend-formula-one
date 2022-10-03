b028af7655257ce7f05d95dc8ccd6e4798f4cb03
pooja allampallewar <poojasa718@gmail.com>
2022-10-03 23:41:58 +0530

    Add yearOptions to constants folder

--- src/components/search.js
+++ src/components/search.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import axios from 'axios'
 import { Divider, Dropdown, Button } from 'semantic-ui-react';
-
+import {yearOptions} from '../constants/years'
 import {
     urlGetBranchAndDegreeData,
     urlGetFilteredStudents,
 } from '../urls'
 
 import '../css/search.css'
-
-const yearOptions = [
-    { key: 1, text: '1st Year', value: 1 },
-    { key: 2, text: '2nd Year', value: 2 },
-    { key: 3, text: '3rd Year', value: 3 },
-    { key: 4, text: '4th Year', value: 4 },
-    { key: 5, text: '5th Year', value: 5 },
-    { key: 6, text: '6th Year', value: 6 },
-    { key: 7, text: '7th Year', value: 7 },
-    { key: 8, text: '8th Year', value: 8 },
-    { key: 9, text: '9th Year', value: 9 },
-    { key: 10, text: '10th Year', value: 10 },
-]
 
 class Search extends React.Component {
     constructor(props) {
